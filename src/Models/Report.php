2068a40a483a113a6c882f147b3b23d22f1439bc
CAR VX Team <it@carvx.jp>
2016-11-10 15:18:53 +1000

    Added creation date and due date to report

--- src/Models/Report.php
+++ src/Models/Report.php
@@ -14,9 +14,13 @@ class Report extends AbstractModel
     const REPORT_ID_FIELD = 'report_id';
     const STATUS_FIELD = 'status';
     const DATA_FIELD = 'data';
+    const CREATION_DATE_FIELD = 'creation_date';
+    const DUE_DATE_FIELD = 'due_date';
 
     public $reportId;
     public $status;
+    public $creationDate;
+    public $dueDate;
     public $data;
 
     public function __construct($reportData)
@@ -34,6 +38,16 @@ class Report extends AbstractModel
         $this->data = $this->isCompleted()
             ? new ReportData($reportData[self::DATA_FIELD])
             : null;
+        $this->creationDate = $this->getValueOrDefault(
+            $reportData,
+            self::CREATION_DATE_FIELD,
+            ''
+        );
+        $this->dueDate = $this->getValueOrDefault(
+            $reportData,
+            self::DUE_DATE_FIELD,
+            ''
+        );
     }
 
     public function isCompleted()
@@ -46,7 +60,9 @@ class Report extends AbstractModel
         return [
             self::REPORT_ID_FIELD => 'reportId',
             self::STATUS_FIELD => 'status',
-            self::DATA_FIELD => 'data'
+            self::DATA_FIELD => 'data',
+            self::CREATION_DATE_FIELD => 'creationDate',
+            self::DUE_DATE_FIELD => 'dueDate',
         ];
     }
 }
